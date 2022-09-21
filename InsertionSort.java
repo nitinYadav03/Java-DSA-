0085214cb5631855a285a8dfd898ff781cead8d7
nitinYadav03 <93430543+nitinYadav03@users.noreply.github.com>
2022-09-21 16:21:09 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,30 @@
+package sortingalgo;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void insertion(int[] arr) {
+        for(int i = 0; i < arr.length - 1; i++){
+            for(int j = i + 1; j > 0; j--){
+                if(arr[j] < arr[j - 1]){
+                    swap(arr, j, j - 1);
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+
+    static void swap(int[] arr, int ind1, int ind2){
+        int temp = arr[ind1];
+        arr[ind1] = arr[ind2];
+        arr[ind2] = temp;
+    }
+
+}
